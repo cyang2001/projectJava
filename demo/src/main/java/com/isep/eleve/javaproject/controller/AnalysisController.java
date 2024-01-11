a84845a6cdf73ce9250acf7cde9ca19706a5e603
remi0 <moveha9654@duiter.com>
2024-01-11 15:36:12 +0100

    update all switch page done les handle... des controller sont à compléter

--- demo/src/main/java/com/isep/eleve/javaproject/controller/AnalysisController.java
+++ demo/src/main/java/com/isep/eleve/javaproject/controller/AnalysisController.java
@@ -1,18 +1,22 @@
 package com.isep.eleve.javaproject.controller;
 
+import com.isep.eleve.javaproject.App;
 import javafx.event.ActionEvent;
 
-public class AnalysisController {
+public class AnalysisController extends App {
 
     public void handleUserInformationAction(ActionEvent event) {
     }
 
-    public void handleDemonstrateValueAction(ActionEvent event) {
+    public void handleDemonstrateValueAction(ActionEvent event) throws Exception {
+        switchToDemonstrateValuePage();
     }
 
-    public void handleAddEventsAction(ActionEvent event) {
+    public void handleAddEventsAction(ActionEvent event) throws Exception {
+        switchToAddEventsPage();
     }
 
-    public void handleDeleteEventsAction(ActionEvent event) {
+    public void handleDeleteEventsAction(ActionEvent event) throws Exception {
+        switchToDeleteEventPage();
     }
 }
