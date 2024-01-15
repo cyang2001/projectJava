d48a36f8a38b3a106ce140653d74e1615744f17c
bin <liuwenbin689@gmail.com>
2024-01-15 15:17:17 +0100

    Updating the chart display code

--- demo/src/main/java/com/isep/eleve/javaproject/model/chart/LineChartUtils.java
+++ demo/src/main/java/com/isep/eleve/javaproject/model/chart/LineChartUtils.java
@@ -4,6 +4,9 @@ import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import com.isep.eleve.javaproject.model.chart.chartTools.*;
+
+import java.util.List;
 
 public class LineChartUtils {
     private LineChart lineChart;
@@ -21,8 +24,23 @@ public class LineChartUtils {
         yAxis.setLabel("shares");
         yAxis.setAnimated(true);
         //Define the name of the form
-        lineChart.setTitle("");
+        lineChart.setTitle("Stock price changes");
     }
-}
+
+
+        public void addDataSeries(List<Point> stockData) {
+            XYChart.Series<String, Number> series = new XYChart.Series<>();
+            series.setName("AAPL Stock Price");
+            //将数据点添加到系列中
+            for (Point dataPoint : stockData) {
+                series.getData().add(new XYChart.Data<>(dataPoint.getDate(), Double.parseDouble(dataPoint.getClosePrice())));
+            }
+
+            // 将系列添加到图表中
+            lineChart.getData().add(series);
+        }
+    }
+
+
 
 
